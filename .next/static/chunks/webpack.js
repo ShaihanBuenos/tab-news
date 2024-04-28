81a027a68d0c640be4a72621cc8f0e9a69cdb727
ShaihanBuenos <shaihanbreg@hotmail.com>
2024-04-28 03:14:22 -0300

    Update ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "54233108db886a0d"; }
+/******/ 		__webpack_require__.h = function() { return "1ec8cd214c4223e8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
